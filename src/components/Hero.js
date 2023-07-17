cec42d11e7b933020a90f4e93494a9e14ec0e3d8
CyberYoghurt <robertocr.21@hotmail.com>
2023-07-17 16:11:59 -0600

    Hero responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ export default function Hero() {
   return (
     <div className="hero">
       <div className="hero__content">
-        <h1 className="hero__name"> Robert's Development Page</h1>
+        <h1 className="hero__name"> Robert's Portfolio Page</h1>
         <h2 className="hero__info">
           Engineer, web developer, network engineer.
         </h2>
